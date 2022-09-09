839c4dd4f222a3c66a49ba3cbaafb6304aa264fe
hiroki-yama-1118 <norishiro1118@gmail.com>
2022-09-09 15:24:56 +0900

    Categoryの作成と編集、削除機能を追加

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -1,14 +1,14 @@
 <x-admin-layout>
   <x-slot name="header">
     <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-      {{ __('Dashboard') }}
+      {{ __("Dashboard") }}
     </h2>
   </x-slot>
 
   <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div class="flex justify-end m-2 p-2">
-        <a href="{{route('admin.categories.create')}}"
+        <a href="{{ route('admin.categories.create') }}"
           class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">New Category</a>
       </div>
       <div class="flex flex-col">
@@ -20,66 +20,49 @@
                   <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
                       <th scope="col" class="py-3 px-6">
-                        Product name
+                        Name
                       </th>
                       <th scope="col" class="py-3 px-6">
-                        Color
+                        Image
                       </th>
                       <th scope="col" class="py-3 px-6">
-                        Category
+                        Discription
                       </th>
                       <th scope="col" class="py-3 px-6">
-                        Price
+
                       </th>
                     </tr>
                   </thead>
                   <tbody>
+                    @foreach ($categories as $category)
                     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                      <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        Apple MacBook Pro 17"
-                      </th>
-                      <td class="py-4 px-6">
-                        Sliver
+                      <td scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        {{ $category->name }}
                       </td>
-                      <td class="py-4 px-6">
-                        Laptop
+                      <td scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        <img src="{{Storage::url($category->image)}}" class="w-16 h-16 rounded" />
                       </td>
-                      <td class="py-4 px-6">
-                        $2999
+                      <td scope=" row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        {{ $category->description }}
                       </td>
-                    </tr>
-                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                      <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        Microsoft Surface Pro
-                      </th>
-                      <td class="py-4 px-6">
-                        White
-                      </td>
-                      <td class="py-4 px-6">
-                        Laptop PC
-                      </td>
-                      <td class="py-4 px-6">
-                        $1999
+                      <td scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        <div class="flex space-x-2">
+                          <a href="{{route('admin.categories.edit',$category->id)}}"
+                            class="px-4 py-2 bg-green-500 hover:bg-green-700 rounded-lg text-white">Edit</a>
+                          <form class="px-4 py-2 bg-red-500 hover:bg-red-700 rounded-lg text-white" method="POST"
+                            action="{{route('admin.categories.destroy',$category->id)}}"
+                            onsubmit="return confirm('Are you sure?')">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit">Delete</button>
+                          </form>
+                        </div>
                       </td>
                     </tr>
-                    <tr class="bg-white dark:bg-gray-800">
-                      <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        Magic Mouse 2
-                      </th>
-                      <td class="py-4 px-6">
-                        Black
-                      </td>
-                      <td class="py-4 px-6">
-                        Accessories
-                      </td>
-                      <td class="py-4 px-6">
-                        $99
-                      </td>
-                    </tr>
+                    @endforeach
                   </tbody>
                 </table>
               </div>
-
             </div>
           </div>
         </div>
